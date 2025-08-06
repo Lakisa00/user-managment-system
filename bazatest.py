784f6f35682b193f060e40a3ce2533f75b9a4b7e
Lakisa00 <radjenlazar@gmail.com>
2025-08-06 22:01:56 +0200

    Added enviorment variables

--- bazatest.py
+++ bazatest.py
@@ -1,10 +1,17 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_NAME = os.getenv("DB_NAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 conn = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="laki1234",
-    database="test"
+    host= DB_HOST,
+    user= DB_USER,
+    password= DB_PASSWORD,
+    database= DB_NAME
 )
 cursor = conn.cursor()
 cursor.execute("""
