7be2e2ad789376cd57ec83f03b93f9758c5f887d
Lakisa00 <radjenlazar@gmail.com>
2025-08-07 22:05:54 +0200

    Added menu and function names

--- library_managment.py
+++ library_managment.py
@@ -24,5 +24,67 @@ create table if not exists books(
 ) 
 """)
 conn.commit()
+
+def add_book(title, author, year):
+    pass
+
+def list_books():
+    pass
+
+def search_books(keyword):
+    pass
+
+def borrow_book(book_id):
+    pass
+
+def return_book(book_id):
+    pass
+
+def delete_book(book_id):
+    pass
+
+while True:
+    print("1: Add Book")
+    print("2: List Books")
+    print("3: Search Books")
+    print("4: Borrow Book")
+    print("5: Return Book")
+    print("6: Delete Book")
+    print("7: EXIT")
+    choice = input('Choose an Option 1-5: ').strip()
+    if choice == '1':
+        title = input("Enter Title: ").strip()
+        author = input("Enter Author: ").strip()
+        year = input("Enter Year: ").strip()
+        year = int(year) if year.isdigit() else None
+        add_book(title, author, year)
+    elif choice == '2':
+        list_books()
+    elif choice == '3':
+        keyword = input("Find Book: ").strip()
+        search_books(keyword)
+    elif choice == '4':
+        book_id = input("Enter Book ID to Borrow: ")
+        if book_id.isdigit():
+            borrow_book(int(book_id))
+        else:
+            print("Invalid")
+    elif choice == '5':
+        book_id = input("Enter Book ID to Return: ")
+        if book_id.isdigit():
+            return_book(int(book_id))
+        else:
+            print("Invalid ID")
+    elif choice == '6':
+        book_id = input("Enter Book ID to Delete Book: ")
+        if book_id.isdigit():
+            delete_book(int(book_id))
+        else:
+            print("Invalid Book ID")
+    elif choice == '7':
+        print('Bye')
+        break
+    else:
+        print("Invalid CHoice Pick the Following Options")
 cursor.close()
 conn.close()
